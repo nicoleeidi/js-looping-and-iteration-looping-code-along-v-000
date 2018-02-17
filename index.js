c9d1f2152afb562890e922a11af867df9ceefb0e
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-17 18:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solutions in this file
 function printBadger(array){
-  for (let i=0; i < array.length; i++)
+  for (let i=0; i < array.length; i++){
+    console.log ("LOG: Welcome " + array[i] + "! You are employee #" + i)
+  }
 }
